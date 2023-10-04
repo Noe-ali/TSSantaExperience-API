239faef34f8a3c771e4901f9d6d4055c93aaea54
Noé Rivera Lugo <tsnoe.dali@gmail.com>
2023-10-03 19:34:28 -0600

    add to main route

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 app.disable('x-powered-by')
 
+//require routers
+import { usersRouter } from './routes/users';
 
 app.get('/', (req, res) => {
     res.status(200).send(`listen on port ${PORT} `);
@@ -13,3 +15,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
 });
+
+
+// Routers
+app.use('/users', usersRouter);
