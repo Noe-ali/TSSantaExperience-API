83aefd8c34a14bea90ed060a46a580efd0fe6df5
Noé Rivera Lugo <tsnoe.dali@gmail.com>
2023-10-04 21:31:24 -0600

    remove unused code

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,20 +1,23 @@
 import { User } from "../schemas/userSchema";
 import { Pool, Client } from "pg";
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "TSSexperience",
-  password: "postgres",
-  port: 5432,
-});
+const pool = new Pool(
+  // {
+  //   user: "postgres",
+  //   host: "localhost",
+  //   database: "TSSexperience",
+  //   password: "postgres",
+  //   port: 5432,
+  // }
+);
 
 export class UserModel {
   static async getAll() {
     const getUsers = await pool.query(
       "SELECT username, password, phone_number FROM users"
     );
-    console.log(getUsers.rows);
+    await pool.end();
+    return getUsers.rows;
   }
 
   // static async getById(id: number): User{
